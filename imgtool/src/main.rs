24516eec142af60c67a30db50924f2fde75da3ab
dsolerh <dsolerh.cinter95@gmail.com>
2024-01-24 15:47:09 +0200

    feat(imgtool): implement the image rotation

--- imgtool/src/main.rs
+++ imgtool/src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let image_path = env::args().skip(1).next().expect("one argument is missing");
+    let path = Path::new(&image_path);
+    let img = image::open(path).expect("the path should be valid");
+    let rotated = img.rotate90();
+    rotated.save(path).expect("the path should be still valid");
 }
