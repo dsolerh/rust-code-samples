c12bf22f54f67f972ec5bd7ff46e6f4a8cf69987
dsolerh <dsolerh.cinter95@gmail.com>
2025-03-06 19:48:35 +0200

    Start aoc project

---
+++ aoc/send-reponse.sh
@@ -0,0 +1,7 @@
+curl 'https://adventofcode.com/2024/day/1/answer' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'content-type: application/x-www-form-urlencoded' \
+  -H 'cookie: session=53616c7465645f5f99e3e6fbae4dccf31d3eb0770bbd7e3e1ea7734937c999f28365edff3c9e773f2c05acd529809f8098775a43f6fa6c2b5f583d13d4961cb5' \
+  -H 'origin: https://adventofcode.com' \
+  -H 'referer: https://adventofcode.com/2024/day/1' \
+  --data-raw 'level=1&answer=12'
--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let client = reqwest::blocking::Client::new();
+    let res = client
+        .get(format!("https://adventofcode.com/{}/day/{}/input", 2024, 1))
+        .send()?;
+
+    let status = res.status();
+    let response = res.text()?;
+    println!("status: {status}");
+    println!("response: {response}");
+
+    Ok(())
+}
+//
