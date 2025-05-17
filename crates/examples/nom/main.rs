b8bdc669df36df0db1337f76e7b78d5155904aee
dsolerh <dsolerh.cinter95@gmail.com>
2025-05-17 04:00:15 +0300

    chore(crates): update nom examples

--- crates/examples/nom/main.rs
+++ crates/examples/nom/main.rs
@@ -1,5 +1,8 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag_no_case;
+use nom::bytes::tag;
+use nom::character::complete::i32 as parse_i32;
+use nom::sequence::separated_pair;
 use nom::{IResult, Parser};
 
 fn parse_base(input: &str) -> IResult<&str, &str> {
@@ -24,4 +27,21 @@ fn main() -> anyhow::Result<()> {
 
     assert!(parse_pair("Dct").is_err());
     Ok(())
+}
+
+fn parse_integer_pair(input: &str) -> IResult<&str, (i32, i32)> {
+    separated_pair(parse_i32, tag(", "), parse_i32).parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("23, 32", (23,32))]
+    fn test_parse_integer_pair(#[case] input: String, #[case] expected: (i32, i32)) {
+        let (_, result) = parse_integer_pair(input.as_str()).unwrap();
+        assert_eq!(result, expected);
+    }
 }
